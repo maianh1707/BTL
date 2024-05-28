da3a8d3cdeeb7362c9162736f1cf1d8e67b14fd8
maithuy2402 <24a4040622@bav.edu.vn>
2024-05-28 21:46:50 +0700

    Add files via upload

---
+++ chatlieu.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+
+namespace baitaplon.Forms
+{
+    public partial class chatlieu : Form
+    {
+        public chatlieu()
+        {
+            InitializeComponent();
+        }
+
+        private void chatlieu_Load(object sender, EventArgs e)
+        {
+            txtmachatlieu.Enabled = false;
+            btnluu.Enabled = false;
+            btnboqua.Enabled = false;
+            Load_DataGridView();
+        }
+        DataTable tblcl;
+        private void Load_DataGridView()
+        {
+            string sql;
+            sql = "SELECT machatlieu,tenchatlieu FROM tblchatlieu";
+            tblcl = Class.Functions.GetDataToTable(sql);
+            dgridchatlieu.DataSource = tblcl;
+            dgridchatlieu.Columns[0].HeaderText = "Mã chất liệu";
+            dgridchatlieu.Columns[1].HeaderText = "Tên chất liệu";
+            dgridchatlieu.Columns[0].Width = 150;
+            dgridchatlieu.Columns[1].Width = 400;
+            dgridchatlieu.AllowUserToAddRows = false;
+            // Không cho phép sửa dữ liệu trực tiếp trên lưới
+            dgridchatlieu.EditMode = DataGridViewEditMode.EditProgrammatically;
+        }
+
+        private void dgridchatlieu_Click(object sender, EventArgs e)
+        {
+            if (btnthem.Enabled == false)
+            {
+                MessageBox.Show("Đang ở chế độ thêm mới!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                txtmachatlieu.Focus();
+                return;
+            }
+            if (tblcl.Rows.Count == 0)
+            {
+                MessageBox.Show("Không có dữ liệu!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+            txtmachatlieu.Text = dgridchatlieu.CurrentRow.Cells["machatlieu"].Value.ToString();
+            txttenchatlieu.Text = dgridchatlieu.CurrentRow.Cells["tenchatlieu"].Value.ToString();
+            btnsua.Enabled = true;
+            btnxoa.Enabled = true;
+            btnboqua.Enabled = true;
+        }
+
+        private void btnthem_Click(object sender, EventArgs e)
+        {
+            btnsua.Enabled = false;
+            btnxoa.Enabled = false;
+            btnboqua.Enabled = true;
+            btnluu.Enabled = true;
+            btnthem.Enabled = false;
+            ResetValues();
+            txtmachatlieu.Enabled = true;
+            txtmachatlieu.Focus();
+        }
+        private void ResetValues()
+        {
+            txtmachatlieu.Text = "";
+            txttenchatlieu.Text = "";
+        }
+
+        private void btnluu_Click(object sender, EventArgs e)
+        {
+            string sql;
+            if (txtmachatlieu.Text == "")
+            {
+                MessageBox.Show("Bạn phải nhập mã chất liệu", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                txtmachatlieu.Focus();
+                return;
+            }
+            if (txttenchatlieu.Text.Trim().Length == 0)
+            {
+                MessageBox.Show("Bạn phải nhập tên chất liệu", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                txttenchatlieu.Focus();
+                return;
+            }
+            sql = "SELECT machatlieu FROM tblchatlieu WHERE machatlieu=N'" + txtmachatlieu.Text.Trim() + "'";
+            if (Class.Functions.CheckKey(sql))
+            {
+                MessageBox.Show("Mã chất liệu này đã có, bạn phải nhập mã khác", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                txtmachatlieu.Focus();
+                txtmachatlieu.Text = "";
+                return;
+            }
+            sql = "INSERT INTO tblchatlieu(machatlieu,tenchatlieu) VALUES(N'" + txtmachatlieu.Text + "',N'" + txttenchatlieu.Text + "')";
+            Class.Functions.RunSql(sql);
+            Load_DataGridView();
+            ResetValues();
+            btnxoa.Enabled = true;
+            btnthem.Enabled = true;
+            btnsua.Enabled = true;
+            btnboqua.Enabled = false;
+            btnluu.Enabled = false;
+            txtmachatlieu.Enabled = false;
+        }
+
+        private void btnsua_Click(object sender, EventArgs e)
+        {
+            string sql;
+            if (tblcl.Rows.Count == 0)
+            {
+                MessageBox.Show("Không còn dữ liệu!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+            if (txtmachatlieu.Text == "")
+            {
+                MessageBox.Show("Bạn chưa chọn bản ghi nào", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+            if (txttenchatlieu.Text.Trim().Length == 0)
+            {
+                MessageBox.Show("Bạn phải nhập tên chất liệu", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                txttenchatlieu.Focus();
+                return;
+            }
+            sql = "UPDATE tblchatlieu SET tenchatlieu=N'" + txttenchatlieu.Text.ToString() + "' WHERE machatlieu=N'" + txtmachatlieu.Text + "'";
+            Class.Functions.RunSql(sql);
+            Load_DataGridView();
+            ResetValues();
+            btnboqua.Enabled = false;
+        }
+
+        private void btnxoa_Click(object sender, EventArgs e)
+        {
+            string sql;
+            if (tblcl.Rows.Count == 0)
+            {
+                MessageBox.Show("Không còn dữ liệu!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+            if (txtmachatlieu.Text == "")
+            {
+                MessageBox.Show("Bạn chưa chọn bản ghi nào", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+            if (MessageBox.Show("Bạn có muốn xóa không?", "Thông báo", MessageBoxButtons.OKCancel, MessageBoxIcon.Question) == DialogResult.OK)
+            {
+                sql = "DELETE tblchatlieu WHERE machatlieu=N'" + txtmachatlieu.Text + "'";
+                Class.Functions.RunSqlDel(sql);
+                Load_DataGridView();
+                ResetValues();
+            }
+        }
+
+        private void btnboqua_Click(object sender, EventArgs e)
+        {
+            ResetValues();
+            btnboqua.Enabled = false;
+            btnthem.Enabled = true;
+            btnxoa.Enabled = true;
+            btnsua.Enabled = true;
+            btnluu.Enabled = false;
+            txtmachatlieu.Enabled = false;
+        }
+
+        private void btndong_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ timkiemsanpham.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using baitaplon.Class;
+
+namespace baitaplon.Forms
+{
+    public partial class timkiemsanpham : Form
+    {
+        public timkiemsanpham()
+        {
+            InitializeComponent();
+        }
+
+        private void timkiemsanpham_Load(object sender, EventArgs e)
+        {
+            ResetValues();
+            dgridtimkiemsanpham.DataSource = null;
+        }
+        private void ResetValues()
+        {
+            foreach (Control Ctl in this.Controls)
+                if (Ctl is TextBox)
+                    Ctl.Text = "";
+            txtmasanpham.Focus();
+        }
+        DataTable tblsp;
+
+        private void btntimkiem_Click(object sender, EventArgs e)
+        {
+            string sql;
+            if ((txtmasanpham.Text == "") && (txttensanpham.Text == "") && (txtmaloai.Text == "") &&
+               (txtmaco.Text == "") && (txtmachatlieu.Text == "") &&
+               (txtmamau.Text == "") && (txtmamua.Text == "") &&
+               (txtmancc.Text == "") && (txtdongianhap.Text == "")&&
+               (txtsoluong.Text == "") &&(txtdongiaban.Text == ""))
+            {
+                MessageBox.Show("Hãy nhập một điều kiện tìm kiếm!!!", "Yeu cau ...",MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            sql = "SELECT * FROM tblsanpham WHERE 1=1";
+            if (txtmasanpham.Text != "")
+                sql = sql + " AND masanpham Like N'%" + txtmasanpham.Text + "%'";
+            if (txttensanpham.Text != "")
+                sql = sql + " AND tensanpham Like N'%" + txttensanpham.Text + "%'";
+            if (txtmaloai.Text != "")
+                sql = sql + " AND maloai Like N'%" + txtmaloai.Text + "%'";
+            if (txtmaco.Text != "")
+                sql = sql + " AND maco Like N'%" + txtmaco.Text + "%'";
+            if (txtmachatlieu.Text != "")
+                sql = sql + " AND machatlieu Like N'%" + txtmachatlieu.Text + "%'";
+            if (txtmamau.Text != "")
+                sql = sql + " AND mamau Like N'%" + txtmamau.Text + "%'";
+            if (txtmamua.Text != "")
+                sql = sql + " AND mamua Like N'%" + txtmamua.Text + "%'";
+            if (txtmancc.Text != "")
+                sql = sql + " AND mancc Like N'%" + txtmancc.Text + "%'";
+            if (txtsoluong.Text != "")
+                sql = sql + " AND soluong <=" + txtsoluong.Text;
+            if (txtdongianhap.Text != "")
+                sql = sql + " AND dongianhap <=" + txtdongianhap.Text;
+            if (txtdongiaban.Text != "")
+                sql = sql + " AND dongiaban <=" + txtdongiaban.Text;
+            tblsp = Functions.GetDataToTable(sql);
+            if (tblsp.Rows.Count == 0)
+            {
+                MessageBox.Show("Không có bản ghi thỏa mãn điều kiện!!!", "Thông báo",MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                ResetValues();
+            }
+            else
+                MessageBox.Show("Có " + tblsp.Rows.Count + " bản ghi thỏa mãn điều kiện!!!","Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            dgridtimkiemsanpham.DataSource = tblsp;
+            Load_DataGridView();
+        }
+        private void Load_DataGridView()
+        {
+            dgridtimkiemsanpham.Columns[0].HeaderText = "Mã sản phẩm";
+            dgridtimkiemsanpham.Columns[1].HeaderText = "Tên sản phẩm";
+            dgridtimkiemsanpham.Columns[2].HeaderText = "Số lượng";
+            dgridtimkiemsanpham.Columns[3].HeaderText = "Ảnh";
+            dgridtimkiemsanpham.Columns[4].HeaderText = "Đơn giá nhập";
+            dgridtimkiemsanpham.Columns[5].HeaderText = "Đơn giá bán";
+            dgridtimkiemsanpham.Columns[6].HeaderText = "Mã loại";
+            dgridtimkiemsanpham.Columns[7].HeaderText = "Mã cỡ";
+            dgridtimkiemsanpham.Columns[8].HeaderText = "Mã chất liệu";
+            dgridtimkiemsanpham.Columns[9].HeaderText = "Mã màu";
+            dgridtimkiemsanpham.Columns[10].HeaderText = "Mã mùa";
+            dgridtimkiemsanpham.Columns[11].HeaderText = "Mã nhà cung cấp";
+            dgridtimkiemsanpham.Columns[0].Width = 80;
+            dgridtimkiemsanpham.Columns[1].Width = 100;
+            dgridtimkiemsanpham.Columns[2].Width = 80;
+            dgridtimkiemsanpham.Columns[3].Width = 80;
+            dgridtimkiemsanpham.Columns[4].Width = 80;
+            dgridtimkiemsanpham.Columns[5].Width = 80;
+            dgridtimkiemsanpham.Columns[6].Width = 80;
+            dgridtimkiemsanpham.Columns[7].Width = 80;
+            dgridtimkiemsanpham.Columns[8].Width = 80;
+            dgridtimkiemsanpham.Columns[9].Width = 80;
+            dgridtimkiemsanpham.Columns[10].Width = 80;
+            dgridtimkiemsanpham.Columns[11].Width = 80;
+            dgridtimkiemsanpham.AllowUserToAddRows = false;
+            dgridtimkiemsanpham.EditMode = DataGridViewEditMode.EditProgrammatically;
+        }
+
+        private void btntimlai_Click(object sender, EventArgs e)
+        {
+            ResetValues();
+            dgridtimkiemsanpham.DataSource = null;
+        }
+
+        private void txtsoluong_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (((e.KeyChar >= '0') && (e.KeyChar <= '9')) || (Convert.ToInt32(e.KeyChar) == 8))
+                e.Handled = false;
+            else
+                e.Handled = true;
+        }
+
+        private void txtdongianhap_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (((e.KeyChar >= '0') && (e.KeyChar <= '9')) || (Convert.ToInt32(e.KeyChar) == 8))
+                e.Handled = false;
+            else
+                e.Handled = true;
+        }
+
+        private void txtdongiaban_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (((e.KeyChar >= '0') && (e.KeyChar <= '9')) || (Convert.ToInt32(e.KeyChar) == 8))
+                e.Handled = false;
+            else
+                e.Handled = true;
+        }
+
+        private void btndong_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        
+    }
+}
+
+
+
